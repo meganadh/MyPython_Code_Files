87465c2607d01c210992f1689d3c9e2a30eecc0a
Meganadha Reddy K <meganadh@gmail.com>
2022-12-14 21:18:48 +0530

    Add files via upload

---
+++ 14Dec2022/MySubjects.py
@@ -0,0 +1,15 @@
+class Mathematics:
+    def add(a,b):
+        return a+b
+    def mul(a,b):
+        return a*b
+    def div(a,b):
+        return a/b
+    def sub(a,b):
+        return a-b
+
+class Chemistry:
+    def benzene():
+        return "C6H6"
+    def methane():
+        return "CH4"
